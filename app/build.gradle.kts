d312836f899cd020f21296d12fbefc9431cc8106
Luis Alberto Garay <programarpara@gmail.com>
2026-09-19 22:30:40 -0300

    chore: bump version to 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.guardaestados"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
